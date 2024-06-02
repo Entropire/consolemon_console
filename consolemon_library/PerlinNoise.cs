bb4f85e0d7eb9de11932d0ea12efec13754f977d
Entropire <36831@ma-web.nl>
2024-06-02 11:08:45 +0200

    .

---
+++ consolemon_library/PerlinNoise.cs
@@ -0,0 +1,21 @@
+﻿namespace consolemon_library
+{
+	internal static class PerlinNoise
+	{
+		public static int[][] GenerateChunk()
+		{
+			int[][] map = new int[16][];
+			for (int i = 0; i < 16; i++)
+			{
+				int[] line = new int[16];
+				for (int j = 0; j < 16; j++)
+				{
+					line[j] = j / i;
+				}
+				map[i] = line;
+			}
+
+			return map;
+		}
+	}
+}
